72136e1437b1c31c72dd217a6f054572f4ca9536
chanala <yai@lensod.com>
2016-11-14 00:04:14 +0700

    add parallax

--- js/main.js
+++ js/main.js
@@ -1,11 +1,16 @@
 $(document).ready(function() {
    
 
-/*
-   $(function() {
-   
-	});
-*/
+    var controller = new ScrollMagic.Controller({ globalSceneOptions: { triggerHook: "onLeave", duration: "200%" } });
+    var controller2 = new ScrollMagic.Controller({ globalSceneOptions: { triggerHook: "onEnter", duration: "200%" } });
+    new ScrollMagic.Scene({ triggerElement: "#sec1" })
+        .setTween("#bg1", { y: "60%", ease: Linear.easeNone })
+        .addTo(controller);
+
+     new ScrollMagic.Scene({ triggerElement: "#sec3" })
+        .setTween("#bg3", { y: "40%", ease: Linear.easeNone })
+        .addTo(controller2);
+
 
 
 });
