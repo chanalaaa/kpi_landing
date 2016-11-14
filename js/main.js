72bbfec41a87edff6dc42d368044029fb51afe58
chanala <yai@lensod.com>
2016-11-14 22:01:54 +0700

    sec5, init sec6

--- js/main.js
+++ js/main.js
@@ -1,17 +1,20 @@
 $(document).ready(function() {
-   
+
 
     var controller = new ScrollMagic.Controller({ globalSceneOptions: { triggerHook: "onLeave", duration: "200%" } });
     var controller2 = new ScrollMagic.Controller({ globalSceneOptions: { triggerHook: "onEnter", duration: "300%" } });
+    var controller3 = new ScrollMagic.Controller({ globalSceneOptions: { triggerHook: "onEnter", duration: "300%" } });
     new ScrollMagic.Scene({ triggerElement: "#sec1" })
         .setTween("#bg1", { y: "60%", ease: Linear.easeNone })
         .addTo(controller);
 
-     new ScrollMagic.Scene({ triggerElement: "#sec3" })
+    new ScrollMagic.Scene({ triggerElement: "#sec3" })
         .setTween("#bg3", { y: "40%", ease: Linear.easeNone })
         .addTo(controller2);
 
-
+    new ScrollMagic.Scene({ triggerElement: "#sec5" })
+        .setTween("#bg5", { y: "40%", ease: Linear.easeNone })
+        .addTo(controller3);
 
 });
 
