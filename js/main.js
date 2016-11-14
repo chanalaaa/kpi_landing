68b85be329059516d7e1cfe72aafbb6fe4b23ecd
chanala <yai@lensod.com>
2016-11-14 10:53:18 +0700

    sec3,sec4 (init) side-center

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
    
 
     var controller = new ScrollMagic.Controller({ globalSceneOptions: { triggerHook: "onLeave", duration: "200%" } });
-    var controller2 = new ScrollMagic.Controller({ globalSceneOptions: { triggerHook: "onEnter", duration: "200%" } });
+    var controller2 = new ScrollMagic.Controller({ globalSceneOptions: { triggerHook: "onEnter", duration: "300%" } });
     new ScrollMagic.Scene({ triggerElement: "#sec1" })
         .setTween("#bg1", { y: "60%", ease: Linear.easeNone })
         .addTo(controller);
