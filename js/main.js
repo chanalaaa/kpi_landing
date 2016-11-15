6bb8b46765cc0bca836a28200b78dbb322b0d350
chanala <yai@lensod.com>
2016-11-15 23:57:49 +0700

    dummy collapse, fixed parallax sec5, detailed a bit

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 
     var controller = new ScrollMagic.Controller({ globalSceneOptions: { triggerHook: "onLeave", duration: "200%" } });
     var controller2 = new ScrollMagic.Controller({ globalSceneOptions: { triggerHook: "onEnter", duration: "300%" } });
-    var controller3 = new ScrollMagic.Controller({ globalSceneOptions: { triggerHook: "onEnter", duration: "300%" } });
+    var controller3 = new ScrollMagic.Controller({ globalSceneOptions: { triggerHook: "onEnter", duration: "250%" } });
     new ScrollMagic.Scene({ triggerElement: "#sec1" })
         .setTween("#bg1", { y: "60%", ease: Linear.easeNone })
         .addTo(controller);
@@ -13,7 +13,7 @@ $(document).ready(function() {
         .addTo(controller2);
 
     new ScrollMagic.Scene({ triggerElement: "#sec5" })
-        .setTween("#bg5", { y: "40%", ease: Linear.easeNone })
+        .setTween("#bg5", { y: "50%", ease: Linear.easeNone })
         .addTo(controller3);
 
 });
