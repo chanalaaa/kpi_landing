eb11f405f9ea85d358874c88a318c6ed6bb30416
Chanala Wilangka <yaicw49@hotmail.com>
2016-12-03 17:00:58 +0700

    large detailed (reponsive  table, parallax, animate sec1 and other shit)

--- js/main.js
+++ js/main.js
@@ -1,24 +1,36 @@
 $(document).ready(function() {
 
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        return;
+    } else {
+        var controller = new ScrollMagic.Controller({ globalSceneOptions: { triggerHook: "onLeave", duration: "200%" } });
+        var controller2 = new ScrollMagic.Controller({ globalSceneOptions: { triggerHook: "onEnter", duration: "250%" } });
+        var controller3 = new ScrollMagic.Controller({ globalSceneOptions: { triggerHook: "onEnter", duration: "300%" } });
+        new ScrollMagic.Scene({ triggerElement: "#sec1" })
+            .setTween("#bg1", { y: "60%", ease: Linear.easeNone })
+            .addTo(controller);
 
-    var controller = new ScrollMagic.Controller({ globalSceneOptions: { triggerHook: "onLeave", duration: "200%" } });
-    var controller2 = new ScrollMagic.Controller({ globalSceneOptions: { triggerHook: "onEnter", duration: "250%" } });
-    var controller3 = new ScrollMagic.Controller({ globalSceneOptions: { triggerHook: "onEnter", duration: "300%" } });
-    new ScrollMagic.Scene({ triggerElement: "#sec1" })
-        .setTween("#bg1", { y: "60%", ease: Linear.easeNone })
-        .addTo(controller);
+        new ScrollMagic.Scene({ triggerElement: "#sec3" })
+            .setTween("#bg3", { y: "30%", ease: Linear.easeNone })
+            .addTo(controller2);
 
-    new ScrollMagic.Scene({ triggerElement: "#sec3" })
-        .setTween("#bg3", { y: "20%", ease: Linear.easeNone })
-        .addTo(controller2);
+        new ScrollMagic.Scene({ triggerElement: "#sec5" })
+            .setTween("#bg5", { y: "50%", ease: Linear.easeNone })
+            .addTo(controller3);
 
-    new ScrollMagic.Scene({ triggerElement: "#sec5" })
-        .setTween("#bg5", { y: "50%", ease: Linear.easeNone })
-        .addTo(controller3);
+    }
 
-    $('a[data-toggle="tab"]').click(function() {
+    $('a[data-toggleTab="sec2"]').click(function() {
         $('.collapse.in').collapse('hide');
     });
+
+
+    setTimeout(function() {
+        $('#card_one').addClass('slideDown');
+        $('#card_two').addClass('slideUp');
+        $('#card_three').addClass('slideDown');
+    }, 800);
+
 
 });
 
